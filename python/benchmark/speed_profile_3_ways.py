1f1dc3f25442203dee75918e4a515d9be05de709
Stas Bekman <stas@stason.org>
2021-04-01 15:39:32 -0700

    benchmark script

---
+++ python/benchmark/speed_profile_3_ways.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import argparse
+
+import math
+
+# first always check that the various ways produce the same result
+assert 2**2 == math.pow(2, 2)
+
+# if randomness is used make sure to set the seed
+
+# the ways being tested
+def way1():
+    for i in range(1000000):
+        x = i**2
+
+def way2():
+    for i in range(1000000):
+        x = math.pow(i, 2)
+
+#### cProfile ####
+# ./speed_profile_3_ways.py -c
+
+import cProfile
+
+def cprofileme():
+    print("--------------- cProfile -----------------")
+    cProfile.run("way1()", sort=-1)
+    cProfile.run("way2()", sort=-1)
+
+#### timeit ####
+# ./speed_profile_3_ways.py -t
+import timeit
+
+def timeme():
+    print("--------------- timeit -----------------")
+    print(f'way1={timeit.Timer("way1()", globals=globals()).timeit(number=1)}')
+    print(f'way2={timeit.Timer("way2()", globals=globals()).timeit(number=1)}')
+
+#### line_profiler ####
+# this one requires a special way to be called
+# pip install line_profiler
+# kernprof -l speed_profile_3_ways.py -l; python -m line_profiler speed_profile_3_ways.py.lprof
+
+def line_profileme():
+    print("--------------- line_profiler -----------------")
+    profile(way1)()
+    profile(way2)()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", action="store_true", help="use line_profiler")
+    parser.add_argument("-c", action="store_true", help="use cProfile")
+    parser.add_argument("-t", action="store_true", help="use timeit")
+    args = parser.parse_args()
+    if args.l  : line_profileme()
+    elif args.c: cprofileme()
+    elif args.t: timeme()
