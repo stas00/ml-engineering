195743968e1ac6fdd1ca0083f8c5d95fbfa20ad3
Stas Bekman <stas@stason.org>
2022-01-29 19:33:17 -0800

    add flockprint

--- pytorch/README.md
+++ pytorch/README.md
@@ -1,3 +1,5 @@
 # pytorch tools
 
 - [pt-checkpoint-shrink.py](./pt-checkpoint-shrink.py) - this script fixes checkpoints which for some reason stored tensors with storage larger than their view at the moment of saving. It clones the current view and re-saves them with just the storage of the current view.
+
+- [multi-gpu-non-interleaved-print.py](./multi-gpu-non-interleaved-print.py) - a `flock`-based wrapper around `print` that prevents messages from getting interleaved when multiple processes print at the same time - which is the case with `torch.distributed` used with multiple-gpus.
---
+++ pytorch/multi-gpu-non-interleaved-print.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# printflock allows one to print in a non-interleaved fashion when printing from multiple procesess.
+# Typically this only the issue within a single node. When processes from different node print their
+# output doesn't get interleaved.
+#
+# this file includes a full example
+#
+# if you have 2 gpus run it as:
+#
+# python -m torch.distributed.run --nproc_per_node=2 multi-gpu-non-interleaved-print.py
+
+
+import fcntl
+def printflock(*args, **kwargs):
+    """
+    non-interleaved print function for using when printing concurrently from many processes,
+    like the case under torch.distributed
+    """
+    with open(__file__, "r") as fh:
+        fcntl.flock(fh, fcntl.LOCK_EX)
+        try:
+            print(*args, **kwargs)
+        finally:
+            fcntl.flock(fh, fcntl.LOCK_UN)
+
+
+
+if __name__ == "__main__":
+
+    import torch.distributed as dist
+    import torch
+    import os
+    local_rank = int(os.environ["LOCAL_RANK"])
+    torch.cuda.set_device(local_rank)
+    dist.init_process_group("nccl")
+
+    world_size = dist.get_world_size()
+    rank = dist.get_rank()
+    printflock(f"This is a very long message from rank {rank} (world_size={world_size})")
