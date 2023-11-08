3fe807502e6b8fe2e09816a42b9dbc2959b91c07
Stas Bekman <stas@stason.org>
2023-11-07 18:41:08 -0800

    improve

--- multi-node/all_reduce_bench.py
+++ multi-node/all_reduce_bench.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python
 
-# this version
+# this version:
 # has been derived from @jeffra's gist: https://gist.github.com/jeffra/b5e80466b4c86be00ea3b6f130fb7a36
-#    which in turn is derived from the logic in https://github.com/NVIDIA/nccl-tests
+# which in turn is derived from the logic in https://github.com/NVIDIA/nccl-tests
 # with contributions from:
 # 1. Indu Thangakrishnan https://github.com/indhub to handle timing correctly using cuda events
-# 2. Ross Whiteman https://github.com/rwightman who suggested to gather results to print from rank 0 to avoid interleaving
+# 2. Ross Whiteman https://github.com/rwightman who suggested to gather results to print from rank 0
+#    to avoid interleaving
 #
-# Important: when you finished running this benchmark you want to pay attention to the busbw result (not
-# algobw) as explained here
+#
+# Important notices:
+# - when you finished running this benchmark you want to pay attention to the busbw result (not
+#   algobw) as explained here https://github.com/NVIDIA/nccl-tests/blob/master/doc/PERFORMANCE.md#bandwidth
+#
+# - currently this benchmark tests a payload of 4GB (M * N * 4) - if your target application uses a
+#   much smaller payload you want to modify N to the target payload. To calculate the payload use
+#   the number of parameters per a single layer multiplied by 2 (bf16/fp16) or 4 (fp32)
+#
+# - If you are wondering whether you need to also run https://github.com/NVIDIA/nccl-tests - I
+#   already validated that I got very similar results with ./build/all_reduce_perf -b 4G -e 4G
+#   (tested with mpirun on 4 nodes). But if you want to test other collectives then nccl-tests it
+#   is. It's also useful if you want to test a range of payloads, e.g. there you'd set -b 8 -e 4G -f
+#   2 and it will test many sizes automatically.
 #
 # To run on 4 nodes:
 #
@@ -26,16 +39,16 @@
 #     --tee 3 \
 #     all_reduce_bench.py
 #
-# note: adapt MASTER_ADDR to rank 0 hostname if it's not a SLURM environment where it's derived automatically
-#
-# Important: when you finished running this benchmark you want to pay attention to the busbw result
-# (not algobw) as they are number of gpus-agnostic as explained here:
-# https://github.com/NVIDIA/nccl-tests/blob/master/doc/PERFORMANCE.md#bandwidth
+# note: adapt MASTER_ADDR to node rank 0's hostname if it's not a SLURM environment where it's derived automatically
 #
 # e.g. example to run with salloc+srun
 # salloc --partition=mypartition --nodes=4 --ntasks-per-node=1 --cpus-per-task=48 --gres=gpu:8 --time=1:00:00 bash
-# srun --gres=gpu:8 --nodes=4 --tasks-per-node=1 python -u -m torch.distributed.run --nproc_per_node=8 --nnodes 4 --rdzv_endpoint $(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1):6000 --rdzv_backend c10d all_reduce_bench.py
-
+# srun --gres=gpu:8 --nodes=4 --tasks-per-node=1 python -u -m torch.distributed.run --nproc_per_node=8 \
+# --nnodes 4 --rdzv_endpoint $(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1):6000 --rdzv_backend \
+# c10d all_reduce_bench.py
+#
+# to do a quick test on 2 gpus
+# python -u -m torch.distributed.run --nproc_per_node=2 --rdzv_endpoint localhost:6000  --rdzv_backend c10d all_reduce_bench.py
 
 import os
 import socket
@@ -44,6 +57,7 @@ import torch.distributed as dist
 
 TRIALS = 5
 
+# these emulate the payload which will become a M * N * 4-sized tensor below
 N = 500000
 M = 2000
 
@@ -55,8 +69,8 @@ def timed_allreduce(mat, id, start_event, end_event):
     torch.cuda.synchronize()
     duration = start_event.elapsed_time(end_event) / 1000
 
-    tput = ((M*N*4*2)/duration)*8 # *2 is for send + receive, *8 for gigabits/second
     size = M * N * 4 # 4 is fp32
+    tput = (size * 2 / duration) * 8 # *2 is for send + receive, *8 for gigabits/second
     n = dist.get_world_size()
     # 2*(n-1)/n correction factor is explained here:
     # https://github.com/NVIDIA/nccl-tests/blob/master/doc/PERFORMANCE.md#allreduce
@@ -99,6 +113,6 @@ def init_processes(local_rank, fn, backend='nccl'):
 
 
 if __name__ == "__main__":
-    rank = int(os.environ["LOCAL_RANK"])
-    print("local_rank: %d" % rank)
-    init_processes(local_rank=rank, fn=run)
+    local_rank = int(os.environ["LOCAL_RANK"])
+    print("local_rank: %d" % local_rank)
+    init_processes(local_rank=local_rank, fn=run)
